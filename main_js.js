87b0d80c6454fba51d7f6fc71c41a3afb84f1bdc
John Doligale <doligale05@gmail.com>
2023-11-16 15:34:54 -0600

    Update
    Finished the final details of account page, and added the orange guy logo to the top. This makes sure there is little to no empty space without reason.
    
    Co-Authored-By: ajmac216 <68157061+ajmac216@users.noreply.github.com>

--- main_js.js
+++ main_js.js
@@ -1,7 +1,30 @@
 // Main page JavaScript
+
 document.addEventListener("DOMContentLoaded", function(event){
 
-    $("#load").delay(2000).fadeOut(200);;
+    $("#load").delay(5000).fadeOut(200);
     
+    $("#phone_account").fadeOut(0);
+    
+    const account = document.getElementById("account");
+    
+    var x = 0;
+    
+    function switch_page(){
+    
+        if(x == 0){
+            $("#phone_home").fadeOut(0);
+            $("#phone_account").fadeIn(0);
+            x = 1;
+        }
+        else{
+            $("#phone_account").fadeOut(0);
+            $("#phone_home").fadeIn(0);
+            x = 0;
+        }
+    
+    }
+    
+    account.addEventListener("click", () => switch_page(x));
     
     });
